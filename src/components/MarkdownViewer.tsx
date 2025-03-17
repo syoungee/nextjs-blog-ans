fa4fd721dd441a5d264db5bcf280a7fa392c71d0
syoungee <sunyoungah69@gmail.com>
2025-03-17 17:51:55 +0900

    이미지 설정

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -3,6 +3,7 @@ import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import Image from 'next/image';
 
 export default function MarkdownViewer({ content }: { content: string }) {
   return (
@@ -22,6 +23,7 @@ export default function MarkdownViewer({ content }: { content: string }) {
               </code>
             );
           },
+          img: (image) => <Image className="w-full max-h-60 object-cover" src={image.src || ''} alt={image.alt || ''} width={500} height={350}></Image>,
         }}
       >
         {content}
