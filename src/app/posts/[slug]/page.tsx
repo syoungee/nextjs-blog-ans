5a0984cb4c09e21ea9132170e564b552627b8886
syoungee <sunyoungah69@gmail.com>
2025-03-17 14:10:10 +0900

    post styling

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,18 +1,30 @@
 import MarkdownViewer from '@/components/MarkdownViewer';
 import { getPostData } from '@/service/posts';
+import Image from 'next/image';
+import { FaCalendarCheck } from 'react-icons/fa';
 
 type Props = {
   params: {
     slug: string;
   };
 };
+
 export default async function PostPage({ params: { slug } }: Props) {
-  const post = await getPostData(slug);
+  const { title, description, date, path, content } = await getPostData(slug);
 
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold">{post.title}</h1>
-      <MarkdownViewer content={post.content} />
-    </div>
+    <article className={'rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4'}>
+      <Image className={'w-full h-1/5 max-h-[500px]'} src={`/images/posts/${path}.png`} alt={title} width={760} height={420} />
+      <section className="flex flex-col p-4">
+        <div className="flex items-center self-end text-sky-600">
+          <FaCalendarCheck className="text-sky-600" />
+          <p className="font-semibold ml-2">{date.toString()}</p>
+        </div>
+        <h1 className="text-4xl font-bold">{title}</h1>
+        <p className="text-xl font-bold">{description}</p>
+        <div className="w-44 border-2 border-sky-600 mt-4 mb-8"></div>
+        <MarkdownViewer content={content} />
+      </section>
+    </article>
   );
 }
