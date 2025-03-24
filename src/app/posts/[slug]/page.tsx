93b7e021035e910ccca25af6d0d8fdaab6bb843f
syoungee <sunyoungah69@gmail.com>
2025-03-24 17:20:36 +0900

    포스트 하단 좌우 페이지

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import AdjacentPostCard from '@/components/AdjacentPostCard';
 import PostContent from '@/components/PostContent';
 import { getPostData } from '@/service/posts';
 import Image from 'next/image';
@@ -10,12 +11,16 @@ type Props = {
 
 export default async function PostPage({ params: { slug } }: Props) {
   const post = await getPostData(slug);
-  const { title, path } = post;
+  const { title, path, next, prev } = post;
 
   return (
     <article className={'rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4'}>
       <Image className={'w-full h-1/5 max-h-[500px]'} src={`/images/posts/${path}.png`} alt={title} width={760} height={420} />
       <PostContent post={post} />
+      <section className="flex shadow-md">
+        {prev && <AdjacentPostCard post={prev} type="prev" />}
+        {next && <AdjacentPostCard post={next} type="next" />}
+      </section>
     </article>
   );
 }
